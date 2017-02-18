63aaaf0b6bca337195581be454b5f132e0d950e0
deloarts <deloarts@outlook.com>
2017-02-18 16:21:52 +0100

    Added admin ID check.

--- ESP_Telegram/ESP_Telegram.ino
+++ ESP_Telegram/ESP_Telegram.ino
@@ -27,15 +27,33 @@ TelegramBOT bot(botToken, botName, botUserName);
 // ##################################################################################################################### 
 void setup() 
 {
+	// Setup the pins
+	pinMode(inputPin, INPUT_PULLUP);
+
+	// Start serial com
+	Serial.begin(115200);
+	Serial.print("\n  Connecting to " + String(SSID) + " ");
+
 	// Set the hostname (otherwise the controller will be named esp8266) and init the esp for WiFi use
 	wifi_station_set_hostname(hostname);
 	WiFi.begin(SSID, password);
+
 	// Wait until it is connected to the network
 	while (WiFi.status() != WL_CONNECTED) 
+	{
 	    delay(500);
+	    Serial.print(".");
+	}
+
+	Serial.println("\n\nWiFi connected.");  
+	Serial.print("  IP address: " + String(WiFi.localIP()) + "\n");
+	Serial.print("  Host name:  " + String(hostname) + "\n");
+	Serial.print("- - - - - - - - - - - - - - - - - - -\n\n");
+	delay(3000);
+
 	// Start the bot and send a message to you, so you know the ESP has booted.
 	bot.begin();
-	bot.sendMessage(adminID, F("ESP8266 online."), "");
+	bot.sendMessage(adminID, "ESP8266 online.", "");
 }
 // #####################################################################################################################
 // ######################################### LOOP ######################################################################
@@ -45,29 +63,51 @@ void loop()
 	// Doorbell button has been pushed
 	if (!digitalRead(inputPin))
 	{
-		bot.sendMessage(adminID, F("Ding Dong"), "");
+		bot.sendMessage(adminID, "Ding Dong", "");
 		delay(10000); 	// Halt for 10 sec, if someone pushes the button more often you'll get
 						// many messages.
 						// You could also use a timer, would, in fact, look more pretty.
 	}
 	// React on an incomming message:
+	/* 	
+		GENERAL
+			bot.message[0][0] ... number of received messages
+			bot.message[0][1] ... code of last message
+		MESSAGE
+			bot.message[i][0] ... update ID
+			bot.message[i][1] ... user ID
+			bot.message[i][2] ... first name
+			bot.message[i][3] ... last name
+			bot.message[i][4] ... user ID
+			bot.message[i][5] ... message
+	*/
 	bot.getUpdates(bot.message[0][1]);
 	for (int i = 1; i < bot.message[0][0].toInt() + 1; i++)
 	{
-		bot.message[i][5] = bot.message[i][5].substring(1, bot.message[i][5].length());
-		if (bot.message[i][5] == "info")
+		// Only the admin is allowed to talk to the bot
+		if (bot.message[i][4] == adminID)
 		{
-			bot.sendMessage(bot.message[i][4], String(F("Hello.")), "");
-			bot.sendMessage(bot.message[i][4], String(F("I am your doorbell.")), "");
+			// extract the message
+			bot.message[i][5] = bot.message[i][5].substring(1, bot.message[i][5].length());
+			if (bot.message[i][5] == "info")
+			{
+				bot.sendMessage(bot.message[i][4], "Hello.", "");
+				bot.sendMessage(bot.message[i][4], "I am your doorbell.", "");
+			}
+			else if ((bot.message[i][5] == "get"))
+			{
+				unsigned long runtime = millis() / 1000;
+				bot.sendMessage(bot.message[i][4], "Runtime: " + String(runtime) + "sec.", "");
+			}
+			else 
+			{
+				bot.sendMessage(bot.message[i][4], "Command unknown.", "");
+			}
 		}
-		else if ((bot.message[i][5] == "get"))
-		{
-			unsigned long runtime = millis() / 1000;
-			bot.sendMessage(bot.message[i][4], F("Runtime: ") + String(runtime) + F("sec."), "");
-		}
+		// If any other person as the admin sends something to your bot, let them know they are not allowed to do so.
 		else 
 		{
-			bot.sendMessage(bot.message[i][4], String(F("Command unknown.")), "");
+			bot.sendMessage(bot.message[i][4], "Access denied.", "");
 		}
 	}
 	bot.message[0][0] = "";
